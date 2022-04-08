96e90427283afbac165a8d835ffa7af39fe79d64
tengkurizki18022001@gmail.com <tengkurizki18022001@gmail.com>
2022-04-08 10:15:21 +0700

    add: adding data

--- app/pages/transactions.php
+++ app/pages/transactions.php
@@ -1,3 +1,35 @@
 <div class="container">
-    <h1>tengku</h1>
+    <h1 class="mb-3">transactions</h1>
+
+    <table class="table table-striped table-bordered">
+        <thead class="table-dark">
+            <tr>
+                <th>No.</th>
+                <th>Untuk</th>
+                <th>Orang</th>
+                <th>Nominal</th>
+                <th>status</th>
+                <th>Jatuh Tempo</th>
+                <th>action</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>1.</td>
+                <td>membeli gorengan</td>
+                <td>rizki</td>
+                <td>Rp.8000</td>
+                <td>
+                    <span class="badge bg-danger">unpaid</span>
+                </td>
+                <td>senin , 28 april 2022</td>
+                <td>
+                    <button class="btn btn-info btn-sm"><i class="bi bi-eye"></i></button>
+                    <button class="btn btn-warning btn-sm"><i class="bi bi-pencil-square"></i></button>
+                    <button class="btn btn-danger btn-sm"><i class="bi bi-trash3"></i></button>
+
+                </td>
+            </tr>
+        </tbody>
+    </table>
 </div>
--- app/templates/header.php
+++ app/templates/header.php
@@ -7,6 +7,7 @@
     <title><?=$page ?? 'Dasboard'?> | Hutang Piutang App</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
     <link rel="stylesheet" href="/aasetCss/style.css">
     <link rel="shortcut icon" href="/app/public/favicon.ico" type="image/x-icon">
     <link rel="stylesheet" href="/app/public/css/style.css">
