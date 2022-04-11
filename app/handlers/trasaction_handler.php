c6a36988453db37f995b103d7410aed0ba962382
tengkurizki18022001@gmail.com <tengkurizki18022001@gmail.com>
2022-04-11 10:01:41 +0700

    add ; dolar titel

--- app/handlers/trasaction_handler.php
+++ app/handlers/trasaction_handler.php
@@ -4,14 +4,17 @@ if (isset($_GET['view'])) {
     switch ($_GET['view'] === 'depts') {
         case 'value':
             $where = 'depts';
+            $title = 'hutang';
             break;
 
         default:
             $where = 'receivables';
+            $title = 'piutang';
             break;
     }
 } else {
     $where = 'depts';
+    $title = 'hutang';
 
 }
 
--- app/pages/transactions/create.php
+++ app/pages/transactions/create.php
@@ -1,3 +1,36 @@
 <div class="container">
-    <h1>create <?=$where?></h1>
+    <h1 class="text-center pt-5 pb-5">create <?=($where === 'depts') ? '- Hutang' : '- Piutang'?></h1>
+    <div class="row">
+        <div class="col-4"></div>
+        <div class="col-4">
+            <form action="" method="post">
+                <div class="mb-3">
+                    <label for="kegunaan" class="form-label">Kegunaan</label>
+                    <input type="text" class="form-control" name="use_for" id="kegunaan" aria-describedby="emailHelp">
+                </div>
+                <div class="mb-3">
+                    <label for="orang" class="form-label">Orang</label>
+                    <input type="text" class="form-control" name="person" id="orang">
+                </div>
+                <div class="mb-3">
+                    <label for="nominal" class="form-label">Nominal</label>
+                    <input type="text" class="form-control" name="nominal" id="nominal">
+                </div>
+                <div class="mb-3">
+                    <label for="waktu_receivable"
+                        class="form-label">Waktu<?=($where === 'depts') ? '- Hutang' : '- Piutang'?></label>
+                    <input type="datetime-local" class="form-control" name="transaction_at" id="waktu_receivable">
+                </div>
+                <div class="mb-3">
+                    <label for="kapan_bayar" class="form-label">kapan bayar</label>
+                    <input type="datetime-local" class="form-control" name="transaction_at" id="kapan_bayar">
+                </div>
+                <button type="submit" class="btn btn-primary">buat
+                    <?=($where === 'depts') ? '- Hutang' : '- Piutang'?></button>
+            </form>
+        </div>
+        <div class="col-4"></div>
+    </div>
+
+
 </div>
