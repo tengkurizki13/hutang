fc9bdd3b6581b9000b30fdf43c29a60ddf31003c
tengkurizki18022001@gmail.com <tengkurizki18022001@gmail.com>
2022-06-03 08:23:28 +0700

    add : dashboard

--- app/pages/dashboard.php
+++ app/pages/dashboard.php
@@ -1,3 +1,79 @@
 <div class="container">
-    <h1>sdas</h1>
+    <h1>Dashboard</h1>
+
+
+    <div class="row mt-5 ">
+        <div class="col-3" style="height:170px;">
+            <div class="card   text-dark  bg-secondary bg-gradient mb-3" style="max-width: 18rem;">
+                <h5 class="card-header text-center">Jumlah Hutang</h5>
+                <div class="card-body">
+                    <h5 class="card-title">Gambar</h5>
+                    <ol>
+                        <li>fulan bin bejo</li>
+                    </ol>
+                    <a href="#" class="btn btn-dark">Detail</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-3" style="height:170px;">
+            <div class="card  text-dark  bg-secondary bg-gradient mb-3" style="max-width: 18rem;">
+                <h5 class="card-header text-center">Jumlah Piutang</h5>
+                <div class="card-body">
+                    <h5 class="card-title">Gambar</h5>
+                    <ol>
+                        <li>fulan bin bejo</li>
+                    </ol>
+                    <a href="#" class="btn btn-dark">Detail</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-3" style="height:170px;">
+            <div class="card  text-dark  bg-secondary mb-3 bg-gradient" style="max-width: 18rem;">
+                <h5 class="card-header text-center">Jumlah TRX Hutang</h5>
+                <div class="card-body">
+                    <h5 class="card-title">Gambar</h5>
+                    <ol>
+                        <li>fulan bin bejo</li>
+                    </ol>
+                    <a href="#" class="btn btn-dark">Detail</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-3" style="height:170px;">
+            <div class="card  text-dark  bg-secondary mb-3 bg-gradient" style="max-width: 18rem;">
+                <h5 class="card-header text-center">Jumlah TRX Hutang</h5>
+                <div class="card-body">
+                    <h5 class="card-title">Gambar</h5>
+                    <ol>
+                        <li>fulan bin bejo</li>
+                    </ol>
+                    <a href="#" class="btn btn-dark">Detail</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-3 mt-5" style="height:170px;">
+            <div class="card   text-dark  bg-secondary bg-gradient mb-3" style="max-width: 18rem;">
+                <h5 class="card-header text-center">Jumlah Hutang</h5>
+                <div class="card-body">
+                    <h5 class="card-title">Gambar</h5>
+                    <ol>
+                        <li>fulan bin bejo</li>
+                    </ol>
+                    <a href="#" class="btn btn-dark">Detail</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-3 mt-5" style="height:170px;">
+            <div class="card   text-dark  bg-secondary bg-gradient mb-3" style="max-width: 18rem;">
+                <h5 class="card-header text-center">Jumlah Hutang</h5>
+                <div class="card-body">
+                    <h5 class="card-title">Gambar</h5>
+                    <ol>
+                        <li>fulan bin bejo</li>
+                    </ol>
+                    <a href="#" class="btn btn-dark">Detail</a>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
